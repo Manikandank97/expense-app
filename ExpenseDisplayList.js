3a50f193e2972ac868a987c9075fd18e7c1246ee
Manikandank97 <manikandank97@gmail.com>
2020-05-10 22:19:29 +0530

    Add files via upload

---
+++ ExpenseDisplayList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+function ExpenseDisplayList({ data, editData }) {
+  return (
+    <Container fluid>
+      <Row className="table-header-container">
+        <Col md={1} xs={2}>
+          S.No
+        </Col>
+        <Col md={2} xs={2}>
+          Date
+        </Col>
+        <Col md={3} xs={3}>
+          Description
+        </Col>
+        <Col md={3} xs={3}>
+          Amount <small className="d-none d-sm-block">(INR)</small>
+        </Col>
+        <Col md={2} xs={2}>
+          Edit
+        </Col>
+      </Row>
+      {data && data.length > 0 ? data.map((result, index) => {
+        return (
+          <React.Fragment key={index}>
+            <Row className='table-data-container'>
+
+              <Col md={1} xs={1}>{index + 1}</Col>
+
+              <Col md={2} xs={3}>{result.addedOnDate.toDateString()}</Col>
+
+              <Col md={3} xs={3}> {result.comment}</Col>
+
+              <Col md={3} xs={3} className={result.type == 'spending' ? 'spend' : 'income'}>{result.amount}</Col>
+
+              <Col md={2} xs={1} className="edit-pointer" onClick={() => editData(result, true)}><i className="fa fa-pencil-square-o fa-lg" /></Col>
+            </Row>
+          </React.Fragment>
+        )
+      }) : ''}
+    </Container>
+  );
+}
+
+export default ExpenseDisplayList;
