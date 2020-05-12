7ea840f76e846f2198ada31b30827369fe7d3d52
Manikandank97 <manikandank97@gmail.com>
2020-05-12 19:45:25 +0530

    Add files via upload

--- src/ExpenseDashboard.js
+++ src/ExpenseDashboard.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import ExpenseDisplayHeader from './ExpenseDisplayHeader';
 import ExpenseDisplayList from './ExpenseDisplayList';
 import AddEditComponents from './AddEditComponents';
 import ExpenseUpdateComponents from './ExpenseUpdateComponents';
+import axios from 'axios'
 
 function ExpenseDashboard() {
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: 'https://cors-anywhere.herokuapp.com/https://3d.bk.tudelft.nl/opendata/cityjson/1.0/DenHaag_01.json',
+    })
+      .then(function (response) {
+        const cityObjectData = response.data.CityObjects;
+        updateCityData(prevState => [...prevState, cityObjectData])
+      });
+  }, []);
 
   useEffect(() => {
     const incomeData = { id: 0, comment: 'Salary from work', amount: '50000', type: 'income', addedOnDate: new Date('05-09-2020') }
@@ -15,6 +26,7 @@ function ExpenseDashboard() {
   const [income, calculateIncome] = useState([]);
   const [updateData, updateDataChange] = useState([]);
   const [initUpdate, updateInitChange] = useState(false);
+  const [cityData, updateCityData] = useState([]);
 
   const editData = (data, bool) => {
     updateDataChange(data)
@@ -36,10 +48,24 @@ function ExpenseDashboard() {
     calculateIncome(prevState => [...prevState, { id: income.length, amount: data.amount, comment: data.comment, type: data.type, addedOnDate: data.addedOnDate }]);
   }
 
+  const cityDetailDisp = Object.keys(cityData).forEach(function (key, index) {
+    return (
+      <React.Fragment key={index}>
+        <Col md={3} xs={1}>{key}</Col>
+        <Col md={2} xs={1}>{cityData[key].type}</Col>
+      </React.Fragment>
+    )
+  });
+
+
   return (
     <Container>
+
+      <Row>
+        {cityDetailDisp}
+      </Row>
       <ExpenseDisplayHeader data={income} />
-      <ExpenseDisplayList data={income} editData={editData} />
+      <ExpenseDisplayList data={income} editData={editData} cityData={cityData} />
       <AddEditComponents addData={submitData} />
       <ExpenseUpdateComponents updateDataSubmit={updateDataSubmit} updateData={updateData} initUpdate={initUpdate} />
     </Container>
